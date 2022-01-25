11e94501610431eb9663723e504572ba195353ed
Katri S <>
2022-01-25 19:07:41 +0200

    Own project setup

--- next.config.js
+++ next.config.js
@@ -1,6 +1,54 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+const withLinaria = require('next-linaria');
 
-module.exports = nextConfig
+// Need more permissive for hot reload and other development stuff to work
+const devStyleCSPAdditions = "'self' 'unsafe-inline'";
+const devCSPAdditions = "; script-src 'self' 'unsafe-eval'";
+const securityHeaders = [
+  {
+    key: 'X-Content-Type-Options',
+    value: 'nosniff',
+  },
+  {
+    key: 'Referrer-Policy',
+    value: 'no-referrer',
+  },
+  {
+    key: 'Content-Security-Policy',
+    value: `default-src 'self'; connect-src 'self'; style-src ${
+      process.env.NODE_ENV === 'development' ? devStyleCSPAdditions : ''
+    } https://fonts.googleapis.com; font-src https://fonts.gstatic.com; img-src 'self' data: ${
+      process.env.NODE_ENV === 'development' ? devCSPAdditions : ''
+    }`,
+  },
+  // For older browsers not supporting CSP such as IE
+  {
+    key: 'X-XSS-Protection',
+    value: '1; mode=block',
+  },
+  {
+    key: 'X-Frame-Options',
+    value: 'SAMEORIGIN',
+  },
+];
+
+module.exports = withLinaria({
+  reactStrictMode: true,
+  i18n: {
+    locales: ['fi', 'en'],
+    defaultLocale: 'fi',
+    localeDetection: false,
+  },
+  images: {
+    domains: ['images.ctfassets.net'],
+  },
+  trailingSlash: true,
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes in your application.
+        source: '/(.*)',
+        headers: securityHeaders,
+      },
+    ];
+  },
+});
---
+++ utils/theme.ts
@@ -0,0 +1,15 @@
+export const theme = {
+  colors: {
+    black: '#000010',
+    white: '#fffffd',
+
+    // Highlight
+    green: '#00bb00',
+
+    // Error
+    red: '#bb0000',
+  },
+  fontFamily: {
+    body: 'Roboto, Helvetica Neue, sans-serif',
+  },
+};
